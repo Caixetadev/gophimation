a4033dd745dde4f58dea00b39dba4bd8535f11a0
Rafael Caixeta <caixetadev@gmail.com>
2023-02-13 17:17:25 -0300

    feat: adding the episode and search functions in the application flow

--- main.go
+++ main.go
@@ -1,12 +1,12 @@
 package main
 
 import (
-	"fmt"
 	"os"
 	"os/exec"
 	"regexp"
 	"strings"
 
+	"github.com/Caixetadev/ani-go/episode"
 	"github.com/gocolly/colly/v2"
 )
 
@@ -14,6 +14,8 @@ func main() {
 	c := colly.NewCollector(
 		colly.AllowedDomains("www.anitube.site", "rr1---sn-gx5auxaxjvhxpgxap-btoe.googlevideo.com", "www.blogger.com"),
 	)
+
+	ep := episode.SelectEpisode()
 
 	c.UserAgent = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36"
 
@@ -30,19 +32,17 @@ func main() {
 	c.OnResponse(func(r *colly.Response) {
 		if triggerHTML {
 			c.OnHTML("html", func(e *colly.HTMLElement) {
-				re := regexp.MustCompile(`https://rr1---sn-gx5auxaxjvhxpgxap-btoe[\S]+?"`)
-				src := re.FindAllStringSubmatch(e.Text, 2)[1]
+				res := regexp.MustCompile(`"https://rr[\S]+?"`)
+				url := res.FindAllStringSubmatch(e.Text, -1)
 
-				url := strings.Join(src, "")
+				urlstring := strings.Join(res.FindAllStringSubmatch(e.Text, -1)[len(url)-1], "")
 
-				cmd := exec.Command("/usr/bin/mpv", strings.TrimRight(url, `"`))
-				fmt.Println(cmd)
+				cmd := exec.Command("mpv", strings.Replace(urlstring, `"`, "", -1))
 				cmd.Stdout = os.Stdout
-
 				cmd.Run()
 			})
 		}
 	})
 
-	c.Visit("https://www.anitube.site/918795/")
+	c.Visit(ep)
 }
