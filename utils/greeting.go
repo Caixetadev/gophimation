a797c9e65a79ba2394d97110642b3d609afe840b
Rafael Caixeta <caixetadev@gmail.com>
2023-02-14 17:58:18 -0300

    refactor: adding constant to welcome string

--- utils/greeting.go
+++ utils/greeting.go
@@ -5,18 +5,20 @@ import (
 	"time"
 )
 
+const MESSAGE = "! Seja bem-vindo de volta ao Gophimation"
+
 func Greeting(userName string) {
 	var message string
 
 	switch hour := time.Now().Hour(); {
 	case hour < 6:
-		message = "Boa madrugada, " + userName + "! Seja bem-vindo de volta ao ANI-GO"
+		message = "Boa madrugada, " + userName + MESSAGE
 	case hour < 12:
-		message = "Bom dia, " + userName + "! Seja bem-vindo de volta ao ANI-GO"
+		message = "Bom dia, " + userName + MESSAGE
 	case hour < 17:
-		message = "Boa tarde, " + userName + "! Seja bem-vindo de volta ao ANI-GO"
+		message = "Boa tarde, " + userName + MESSAGE
 	default:
-		message = "Boa noite, " + userName + "! Seja bem-vindo de volta ao ANI-GO"
+		message = "Boa noite, " + userName + MESSAGE
 	}
 
 	fmt.Println(message)
