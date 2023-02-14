66eab01a9f4a2579726dd989aebbc509e17e0569
Rafael Caixeta <caixetadev@gmail.com>
2023-02-14 17:57:26 -0300

    feat: add cache

--- config/colly.go
+++ config/colly.go
@@ -1,9 +1,12 @@
 package config
 
-import "github.com/gocolly/colly/v2"
+import (
+	"github.com/gocolly/colly/v2"
+)
 
 func Colly() *colly.Collector {
 	c := colly.NewCollector(
+		colly.CacheDir("./cache"),
 		colly.AllowedDomains("www.anitube.site", "rr1---sn-gx5auxaxjvhxpgxap-btoe.googlevideo.com", "www.blogger.com"),
 	)
 
