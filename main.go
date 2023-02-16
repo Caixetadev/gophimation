39317fe90a33651f13c62dde1373b5d8dc72aa23
Rafael Caixeta <caixetadev@gmail.com>
2023-02-15 23:27:07 -0300

    feat: adding init function to lengthen user

--- main.go
+++ main.go
@@ -1,19 +1,78 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
+	"log"
 	"os"
 	"os/exec"
 	"regexp"
 	"strings"
 
-	"github.com/Caixetadev/ani-go/episode"
+	"github.com/Caixetadev/gophimation/config"
+	"github.com/Caixetadev/gophimation/episode"
+	"github.com/Caixetadev/gophimation/utils"
 	"github.com/gocolly/colly/v2"
 )
 
+const FILE_NAME = "dataUser.json"
+
+type UserConfig struct {
+	Name string `json:"name"`
+}
+
+func init() {
+	var name string
+
+	utils.Clear()
+
+	_, error := os.Stat(FILE_NAME)
+
+	if os.IsNotExist(error) {
+		fmt.Println("Qual o seu nome?")
+
+		fmt.Scanln(&name)
+
+		file, err := os.Create(FILE_NAME)
+
+		data := UserConfig{
+			Name: name,
+		}
+
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		defer file.Close()
+
+		user, _ := json.Marshal(data)
+
+		_ = os.WriteFile(FILE_NAME, user, 0644)
+
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		utils.Clear()
+	} else {
+		data, err := os.ReadFile(FILE_NAME)
+
+		var user UserConfig
+
+		json.Unmarshal(data, &user)
+
+		utils.Greeting(user.Name)
+
+		fmt.Println()
+
+		if err != nil {
+			log.Panicf("failed reading data from file: %s", err)
+		}
+	}
+}
+
 func main() {
-	c := colly.NewCollector(
-		colly.AllowedDomains("www.anitube.site", "rr1---sn-gx5auxaxjvhxpgxap-btoe.googlevideo.com", "www.blogger.com"),
-	)
+	c := config.Colly()
 
 	ep := episode.SelectEpisode()
 
@@ -37,7 +96,9 @@ func main() {
 
 				urlstring := strings.Join(res.FindAllStringSubmatch(e.Text, -1)[len(url)-1], "")
 
-				cmd := exec.Command("mpv", strings.Replace(urlstring, `"`, "", -1))
+				utils.Clear()
+
+				cmd := exec.Command("mpv", strings.Replace(urlstring, `"`, "", -1), "--demuxer-max-bytes=1G", "--no-terminal", "--fs", "video")
 				cmd.Stdout = os.Stdout
 				cmd.Run()
 			})
