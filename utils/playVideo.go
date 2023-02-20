7a5f9514dfa0467771d3aebcc0a61ee96d5a1bb3
Rafael Caixeta <caixetadev@gmail.com>
2023-02-19 21:21:10 -0300

    refactor: create function to get video

--- utils/playVideo.go
+++ utils/playVideo.go
@@ -7,7 +7,7 @@ import (
 )
 
 func PlayVideo(videoUrl string) {
-	cmd := exec.Command("mpv", "--save-position-on-quit", videoUrl, "--no-terminal", "--fs")
+	cmd := exec.Command("mpv", "--save-position-on-quit", "--no-terminal", "--fs", "--force-media-title=caixetachan", "--cache=yes", videoUrl)
 	cmd.Stdout = os.Stdout
 
 	if err := cmd.Run(); err != nil {
