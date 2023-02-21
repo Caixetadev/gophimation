92267e21b4bd201f9a27513ebe1204543518261c
Rafael Caixeta <caixetadev@gmail.com>
2023-02-21 17:04:22 -0300

    feat: add name episode in mpv

--- utils/playVideo.go
+++ utils/playVideo.go
@@ -1,13 +1,14 @@
 package utils
 
 import (
+	"fmt"
 	"log"
 	"os"
 	"os/exec"
 )
 
-func PlayVideo(videoUrl string) {
-	cmd := exec.Command("mpv", "--save-position-on-quit", "--no-terminal", "--fs", "--force-media-title=caixetachan", "--cache=yes", videoUrl)
+func PlayVideo(videoUrl, nameEpisode string) {
+	cmd := exec.Command("mpv", "--save-position-on-quit", "--no-terminal", "--fs", fmt.Sprintf("--force-media-title=%v", nameEpisode), "--cache=yes", videoUrl)
 	cmd.Stdout = os.Stdout
 
 	if err := cmd.Run(); err != nil {
