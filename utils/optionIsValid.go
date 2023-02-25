4223cf2141fc42632187e37f66eba2d903233515
Rafael Caixeta <caixetadev@gmail.com>
2023-02-25 19:46:03 -0300

    doc: add doc for optionisvalid

--- utils/optionIsValid.go
+++ utils/optionIsValid.go
@@ -4,8 +4,11 @@ import (
 	"log"
 )
 
+// OptionIsValid checks if the option selected by the user is valid for a given list of animes.
 func OptionIsValid(animes []AnimeInfo, option int) {
+	// Checks whether the selected option is invalid.
 	if option > len(animes) || option < 1 {
+		// Clears the screen and exits the program with an error message.
 		Clear()
 		log.Fatalf("Digite um número válido")
 	}
