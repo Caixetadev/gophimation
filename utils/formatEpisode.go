8b094f94952c8bb383698486f6e4592d19c8726c
Rafael Caixeta <caixetadev@gmail.com>
2023-02-25 20:02:50 -0300

    doc: add doc for FormatEpisode

--- utils/formatEpisode.go
+++ utils/formatEpisode.go
@@ -2,6 +2,9 @@ package utils
 
 import "fmt"
 
+// FormatEpisode is a function that takes an integer episode number and formats it as a string.
+// It checks if the episode number is less than 10 and adds a leading zero for format consistency.
+// The function returns the formatted episode string.
 func FormatEpisode(episode int) string {
 	var watching string
 
