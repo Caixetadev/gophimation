1f8cfbb6fa258a6b236c723346010938639d3f71
Rafael Caixeta <caixetadev@gmail.com>
2023-03-07 21:01:40 -0300

    refactor: change the condition

--- pkg/util/optionIsValid.go
+++ pkg/util/optionIsValid.go
@@ -7,7 +7,7 @@ import (
 // OptionIsValid checks if the option selected by the user is valid for a given list of anime.
 func OptionIsValid(anime []AnimeInfo, option int) {
 	// Checks whether the selected option is invalid.
-	if option > len(anime) || option < 1 {
+	if option <= 0 || option > len(anime) {
 		// Clears the screen and exits the program with an error message.
 		Clear()
 		log.Fatalf("Digite um número válido")
