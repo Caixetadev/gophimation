e53a9ed146a1db3586e728cedfc36ece6be762f5
Rafael Caixeta <caixetadev@gmail.com>
2023-03-08 02:23:42 -0300

    refactor: removing webscrapping and adding the api

--- pkg/selectVideo/selectVideo.go
+++ pkg/selectVideo/selectVideo.go
@@ -2,86 +2,28 @@ package selectVideo
 
 import (
 	"encoding/json"
-	"io"
 	"log"
-	"regexp"
-	"strings"
+	"net/http"
 
-	"github.com/Caixetadev/gophimation/configs"
 	"github.com/Caixetadev/gophimation/pkg/util"
-	"github.com/gocolly/colly/v2"
 )
-
-type VideoSource struct {
-	Src   string `json:"src"`
-	Label string `json:"label"`
-}
-
-type ApiResponse struct {
-	Data []VideoSource `json:"data"`
-}
 
 // SelectVideo does the search for the url of the video
 func SelectVideo(ep, nameAnime string) {
-	c := configs.Colly()
+	resp, err := http.Get("http://localhost:8000/player/" + ep)
 
-	client := configs.Http()
-
-	var triggerHTML bool
-
-	c.OnHTML("#my-video", func(e *colly.HTMLElement) {
-		URL := e.Attr("data-video-src")
-
-		res, err := client.Get(URL)
-
-		if err != nil {
-			log.Fatal("error")
-		}
-
-		body, err := io.ReadAll(res.Body)
-		if err != nil {
-			log.Fatalln(err)
-		}
-
-		defer res.Body.Close()
-
-		var response ApiResponse
-		err = json.Unmarshal(body, &response)
-		if err != nil {
-			log.Fatalln(err)
-		}
-
-		util.Clear()
-
-		util.PlayVideo(response.Data[len(response.Data)-1].Src, nameAnime)
-	})
-
-	c.OnHTML("#div_video iframe", func(h *colly.HTMLElement) {
-		URL := h.Attr("src")
-
-		triggerHTML = true
-
-		if err := c.Visit(URL); err != nil {
-			log.Fatalln(err)
-		}
-	})
-
-	c.OnResponse(func(r *colly.Response) {
-		if triggerHTML {
-			c.OnHTML("html", func(e *colly.HTMLElement) {
-				res := regexp.MustCompile(`"https://rr[\S]+?"`)
-				url := res.FindAllStringSubmatch(e.Text, -1)
-
-				urlstring := strings.Join(res.FindAllStringSubmatch(e.Text, -1)[len(url)-1], "")
-
-				util.Clear()
-
-				util.PlayVideo(strings.Replace(urlstring, `"`, "", -1), nameAnime)
-			})
-		}
-	})
-
-	if err := c.Visit(ep); err != nil {
-		log.Fatalln(err)
+	if err != nil {
+		log.Fatal(err)
 	}
+
+	defer resp.Body.Close()
+
+	var anime []util.AnimeInfo
+	err = json.NewDecoder(resp.Body).Decode(&anime)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	util.PlayVideo(anime[0].ID, anime[0].Name)
 }
