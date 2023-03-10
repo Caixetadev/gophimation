4aa4e2f53f51e13424f07f2cb825c1f414516c22
Rafael Caixeta <caixetadev@gmail.com>
2023-03-10 02:36:19 -0300

    feat: uncommenting the init function

--- cmd/gophimation/main.go
+++ cmd/gophimation/main.go
@@ -1,27 +1,30 @@
 package main
 
+// Fazer opção para voltar para a lista de animes.
+
 import (
 	"os"
 
+	"github.com/Caixetadev/gophimation/pkg/constants"
 	"github.com/Caixetadev/gophimation/pkg/episode"
 	mostWatched "github.com/Caixetadev/gophimation/pkg/mostWatched"
+	"github.com/Caixetadev/gophimation/pkg/presence"
 	"github.com/Caixetadev/gophimation/pkg/search"
 	"github.com/Caixetadev/gophimation/pkg/selectVideo"
+	"github.com/Caixetadev/gophimation/pkg/util"
 )
 
-// func init() {
-// 	presence.Presence("Caixeta", "https://www.stickersdevs.com.br/wp-content/uploads/2022/01/gopher-adesivo-sticker.png", "Explorando Animes", "Encontre seu próximo anime favorito <3", "")
+func init() {
+	presence.Presence("Caixeta", "https://www.stickersdevs.com.br/wp-content/uploads/2022/01/gopher-adesivo-sticker.png", "Explorando Animes", "Encontre seu próximo anime favorito <3", "")
 
-// 	util.Clear()
+	_, error := os.Stat(constants.FILE_NAME)
 
-// 	_, error := os.Stat(constants.FILE_NAME)
-
-// 	if os.IsNotExist(error) {
-// 		util.CreateFile(constants.FILE_NAME)
-// 	} else {
-// 		util.ReadFile(constants.FILE_NAME)
-// 	}
-// }
+	if os.IsNotExist(error) {
+		util.CreateFile(constants.FILE_NAME)
+	} else {
+		util.ReadFile(constants.FILE_NAME)
+	}
+}
 
 func main() {
 	if len(os.Args) == 1 {
