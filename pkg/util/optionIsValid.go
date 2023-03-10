919b81c1f03be538ea0234beb293cf495704c98c
Rafael Caixeta <caixetadev@gmail.com>
2023-03-10 02:32:47 -0300

    feat: update function clear

--- pkg/util/optionIsValid.go
+++ pkg/util/optionIsValid.go
@@ -11,7 +11,6 @@ func OptionIsValid(anime []models.Anime, option int) {
 	// Checks whether the selected option is invalid.
 	if option <= 0 || option > len(anime) {
 		// Clears the screen and exits the program with an error message.
-		Clear()
 		log.Fatalf("Digite um número válido")
 	}
 }
