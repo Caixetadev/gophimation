18f3a46833fa274301c10f52280d9b2b7bcfac8d
Rafael Caixeta <caixetadev@gmail.com>
2023-03-10 02:33:34 -0300

    feat: add option to start mpv in windows

--- pkg/util/playVideo.go
+++ pkg/util/playVideo.go
@@ -5,13 +5,25 @@ import (
 	"log"
 	"os"
 	"os/exec"
+	"runtime"
 
 	"github.com/Caixetadev/gophimation/pkg/presence"
 )
 
 // PlayVideo runs a command that opens a video player to play a video from a specified URL
 func PlayVideo(videoUrl, nameEpisode string) {
-	cmd := exec.Command("mpv", "--save-position-on-quit", "--no-terminal", "--fs", fmt.Sprintf("--force-media-title=%v", nameEpisode), "--cache=yes", videoUrl)
+	var playerFunction string
+
+	switch runtime.GOOS {
+	case "windows":
+		playerFunction = "mpv.exe"
+	case "linux":
+		playerFunction = "mpv"
+	default:
+		log.Fatalf("Player function not supported on %s", runtime.GOOS)
+	}
+
+	cmd := exec.Command(playerFunction, "--save-position-on-quit", "--no-terminal", "--fs", fmt.Sprintf("--force-media-title=%v", nameEpisode), "--cache=yes", videoUrl)
 	cmd.Stdout = os.Stdout
 
 	if err := cmd.Run(); err != nil {
