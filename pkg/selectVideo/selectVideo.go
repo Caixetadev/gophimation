f67f60d2b2639c5c9fe388f8761267f5ad874288
Rafael Caixeta <caixetadev@gmail.com>
2023-03-11 13:26:48 -0300

    feat: add cache

--- pkg/selectVideo/selectVideo.go
+++ pkg/selectVideo/selectVideo.go
@@ -1,7 +1,9 @@
 package selectVideo
 
 import (
+	"encoding/json"
 	"fmt"
+	"log"
 	"regexp"
 	"strings"
 
@@ -9,6 +11,7 @@ import (
 	"github.com/Caixetadev/gophimation/pkg/constants"
 	"github.com/Caixetadev/gophimation/pkg/util"
 	"github.com/gocolly/colly"
+	"github.com/peterbourgon/diskv/v3"
 )
 
 type PlayerInfo struct {
@@ -18,11 +21,34 @@ type PlayerInfo struct {
 
 // SelectVideo does the search for the url of the video
 func SelectVideo(ep string) {
+	var urlPlayer []PlayerInfo
+
+	// Initialize a new diskv store, rooted at "cache-dir", with a 10MB cache.
+	d := diskv.New(diskv.Options{
+		BasePath:     util.GetHomeDir("gophimation"),
+		Transform:    func(s string) []string { return []string{} },
+		CacheSizeMax: 10 * 1024 * 1024,
+	})
+
+	key := strings.ReplaceAll(strings.ReplaceAll(ep, "-", "_"), "/", "_")
+
+	data, _ := d.Read(key)
+
+	if len(data) > 1 {
+		err := json.Unmarshal(data, &urlPlayer)
+
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		util.PlayVideo(urlPlayer[0].Url, urlPlayer[0].Name)
+
+		return
+	}
+
 	c := configs.Colly()
 
 	iframeURL, nameAnimeAndEpisode := util.GetIframe(constants.URL_BASE + ep)
-
-	var urlPlayer []PlayerInfo
 
 	c.OnRequest(func(r *colly.Request) {
 		r.Headers.Set("Referer", constants.URL_BASE)
@@ -42,6 +68,17 @@ func SelectVideo(ep string) {
 	})
 
 	c.Visit(iframeURL)
+
+	jsonBytes, err := json.Marshal(urlPlayer)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Write the data to the cache.
+	if err := d.Write(key, jsonBytes); err != nil {
+		log.Fatal(err)
+	}
 
 	util.PlayVideo(urlPlayer[0].Url, urlPlayer[0].Name)
 }
