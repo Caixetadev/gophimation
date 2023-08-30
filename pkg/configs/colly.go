07aef4951b39483845b2230ae9d6912772cef976
Rafael Caixeta <caixetadev@gmail.com>
2023-08-30 11:59:11 +0200

    feat: remove cookie session

--- pkg/configs/colly.go
+++ pkg/configs/colly.go
@@ -30,20 +30,13 @@ func Colly() *colly.Collector {
 	}
 
 	cookie := &http.Cookie{
-		Name:   "betteranime_session",
-		Value:  "eyJpdiI6IncrYXdVUHVpTGpoRi9nOTdFazB4N3c9PSIsInZhbHVlIjoic0w4eXYrTEpJL1R6Nkx4TjRtaEZWeFFpamhNTUk3VStSd1ZzRXh5MUZodDBRZ1VZV2N2ejdlRlF4V293RlBydHZTaHpETnRXUEdCOGMyN0haM1dnTTgzc0YwNGVGRDhEYnRPT2JkS1dEbExDYlA1UlZCUmVOUER6VWxxUmNhRngiLCJtYWMiOiIyNmRkNGM5MTUyYTliMWEzN2I2ZjIzM2VlYWY2MzRlZmRiZWQ5ZmMxYmU2OTU1OWE5ZjJlNDJiZDMxNTE0OWJjIiwidGFnIjoiIn0%3D",
-		Domain: "betteranime.net",
-		Path:   "/",
-	}
-
-	cookie2 := &http.Cookie{
 		Name:   "BetterQuality",
 		Value:  "eyJpdiI6IlhNbnFZbVMwV2pHK2xNU1FBTUd1dEE9PSIsInZhbHVlIjoibmh4bVVBT1h2TVBJaEFXSzI3Q2xlUVlNMWxZTlpOSEZtUHBtcVlxa0hDSlRteHpQS083ZmpDQ2xramFOc29NciIsIm1hYyI6Ijg2NzA3M2ZkZDQ4MWY3YzgxNjIzZDQxZjViYzJhNGRjNGY5MTk3NzVhNzI2NWJkYzUxOGNiMDdmYzQyNTA2NGUiLCJ0YWciOiIifQ%3D%3D",
 		Domain: "betteranime.net",
 		Path:   "/",
 	}
 
-	cookieJar.SetCookies(url, []*http.Cookie{cookie, cookie2})
+	cookieJar.SetCookies(url, []*http.Cookie{cookie})
 
 	return c
 }
