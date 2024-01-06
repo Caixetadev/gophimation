fcad13370e8aaae4f31b51a0b3c90383b4435be1
Rafael Caixeta <caixetadev@gmail.com>
2024-01-06 23:06:47 +0100

    refactor: episode scraper

--- pkg/scrapers/episode.go
+++ pkg/scrapers/episode.go
@@ -13,40 +13,23 @@ import (
 	"github.com/gocolly/colly"
 )
 
+const userPromptEpisode = "\nColoque um numero para assistir"
+
 func SelectEpisode(URL string) (string, *string) {
+	c := config.Colly()
+
 	var selectedOption int
 	var episodes []entity.Anime
-
 	var nameAnime string
 	var image string
 
-	c := config.Colly()
-
-	c.OnHTML(".infos_left .anime-info", func(h *colly.HTMLElement) {
-		nameAnime = h.ChildText("h2")
-
-		if URL == "random" {
-			fmt.Printf("O anime random é: %s\n\n", nameAnime)
-		}
-	})
-
-	c.OnHTML("#episodesList .list-group-item-action", func(h *colly.HTMLElement) {
-		fmt.Printf("[%02d] - %v\n", h.Index+1, h.ChildText("a h3"))
-
-		episodes = append(episodes, entity.Anime{Name: h.ChildText("a h3"), URL: strings.TrimPrefix(h.ChildAttr("a", "href"), constants.URL_BASE)})
-	})
-
-	c.OnHTML("main.container", func(h *colly.HTMLElement) {
-		image = h.ChildAttr(".infos-img img", "src")
-	})
+	setCollyCallbacksEpisodes(c, &nameAnime, &episodes, &image, URL)
 
 	if err := c.Visit(constants.URL_BASE + URL); err != nil {
 		log.Fatal(err)
 	}
 
-	fmt.Println("\ncoloque um numero para assistir to aqui")
-
-	fmt.Scanln(&selectedOption)
+	selectedOption = utils.GetUserInput(userPromptEpisode)
 
 	utils.OptionIsValid(episodes, selectedOption)
 
@@ -54,11 +37,35 @@ func SelectEpisode(URL string) (string, *string) {
 
 	fmt.Println("Carregando...")
 
-	go presence.Presence("https:"+image, nameAnime, fmt.Sprintf("Episódio %02d", selectedOption), "https://www.stickersdevs.com.br/wp-content/uploads/2022/01/gopher-adesivo-sticker.png")
+	updatePresence(image, nameAnime, selectedOption)
 
 	if selectedOption == len(episodes) {
 		return episodes[selectedOption-1].URL, nil
 	}
 
 	return episodes[selectedOption-1].URL, &episodes[(selectedOption+1)-1].URL
+}
+
+func setCollyCallbacksEpisodes(c *colly.Collector, nameAnime *string, episodes *[]entity.Anime, image *string, URL string) {
+	c.OnHTML(".infos_left .anime-info", func(h *colly.HTMLElement) {
+		*nameAnime = h.ChildText("h2")
+
+		if URL == "random" {
+			fmt.Printf("O anime random é: %s\n\n", *nameAnime)
+		}
+	})
+
+	c.OnHTML("#episodesList .list-group-item-action", func(h *colly.HTMLElement) {
+		fmt.Printf("[%02d] - %v\n", h.Index+1, h.ChildText("a h3"))
+
+		*episodes = append(*episodes, entity.Anime{Name: h.ChildText("a h3"), URL: strings.TrimPrefix(h.ChildAttr("a", "href"), constants.URL_BASE)})
+	})
+
+	c.OnHTML("main.container", func(h *colly.HTMLElement) {
+		*image = h.ChildAttr(".infos-img img", "src")
+	})
+}
+
+func updatePresence(image string, nameAnime string, selectedOption int) {
+	go presence.Presence("https:"+image, nameAnime, fmt.Sprintf("Episódio %02d", selectedOption), "https://www.stickersdevs.com.br/wp-content/uploads/2022/01/gopher-adesivo-sticker.png")
 }
