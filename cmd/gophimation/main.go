10b76cd7c0aa197f2580c618f01d7352ca63a5cc
Rafael Caixeta <caixetadev@gmail.com>
2024-01-06 22:29:03 +0100

    refactor: import path correctly

--- cmd/gophimation/main.go
+++ cmd/gophimation/main.go
@@ -5,51 +5,47 @@ package main
 import (
 	"os"
 
+	"github.com/Caixetadev/gophimation/internal/presence"
+	"github.com/Caixetadev/gophimation/internal/utils"
 	"github.com/Caixetadev/gophimation/pkg/constants"
-	"github.com/Caixetadev/gophimation/pkg/episode"
-	"github.com/Caixetadev/gophimation/pkg/mostWatched"
-	"github.com/Caixetadev/gophimation/pkg/presence"
-	"github.com/Caixetadev/gophimation/pkg/random"
-	"github.com/Caixetadev/gophimation/pkg/search"
-	"github.com/Caixetadev/gophimation/pkg/selectVideo"
-	"github.com/Caixetadev/gophimation/pkg/util"
+	"github.com/Caixetadev/gophimation/pkg/scrapers"
 )
 
 func init() {
 	go presence.Presence("https://www.stickersdevs.com.br/wp-content/uploads/2022/01/gopher-adesivo-sticker.png", "Explorando Animes", "Encontre seu próximo anime favorito <3", "")
 
-	pathFile := util.GetHomeDir(constants.FILE_NAME)
+	pathFile := utils.GetHomeDir(constants.FILE_NAME)
 
 	_, error := os.Stat(pathFile)
 
 	if os.IsNotExist(error) {
-		util.CreateFile(pathFile)
+		utils.CreateFile(pathFile)
 	} else {
-		util.ReadFile(pathFile)
+		utils.ReadFile(pathFile)
 	}
 }
 
 func main() {
 	switch {
 	case len(os.Args) > 1 && os.Args[1] == "random":
-		random.Random()
+		scrapers.Random()
 
 	case len(os.Args) > 1:
-		animeSearch := search.Search()
-		episodeSelected, nextEpisode := episode.SelectEpisode(animeSearch)
-		videoSelected := selectVideo.SelectVideo(episodeSelected)
+		animeSearch := scrapers.Search()
+		episodeSelected, nextEpisode := scrapers.SelectEpisode(animeSearch)
+		videoSelected := scrapers.SelectVideo(episodeSelected)
 		if nextEpisode != nil {
-			go selectVideo.SelectVideo(*nextEpisode)
+			go scrapers.SelectVideo(*nextEpisode)
 		}
-		util.PlayVideo(videoSelected.Url, videoSelected.Name)
+		utils.PlayVideo(videoSelected.Url, videoSelected.Name)
 	default:
-		animeMostWatched := mostWatched.MostWatched()
-		episodeSelected, nextEpisode := episode.SelectEpisode(animeMostWatched)
-		videoSelected := selectVideo.SelectVideo(episodeSelected)
+		animeMostWatched := scrapers.MostWatched()
+		episodeSelected, nextEpisode := scrapers.SelectEpisode(animeMostWatched)
+		videoSelected := scrapers.SelectVideo(episodeSelected)
 		if nextEpisode != nil {
-			go selectVideo.SelectVideo(*nextEpisode)
+			go scrapers.SelectVideo(*nextEpisode)
 		}
-		util.PlayVideo(videoSelected.Url, videoSelected.Name)
+		utils.PlayVideo(videoSelected.Url, videoSelected.Name)
 	}
 
 }
