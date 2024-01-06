08999c141d2aed2c64b10eb0a961792ca09d8e3f
Rafael Caixeta <caixetadev@gmail.com>
2024-01-06 22:28:35 +0100

    feat: add presence

---
+++ internal/presence/presence.go
@@ -0,0 +1,45 @@
+package presence
+
+import (
+	"errors"
+	"fmt"
+	"time"
+
+	"github.com/hugolgst/rich-go/client"
+)
+
+const CLIENT_ID = "1075841986923352079"
+
+// Presence sets the presence status of a Discord client with a custom activity
+func Presence(animeImage, animeName, state, smallImage string) {
+	err := client.Login(CLIENT_ID)
+
+	if err != nil {
+		err := errors.New("Aviso: Não foi possivel ativar a conexao com o discord\n")
+		fmt.Println(err)
+	}
+
+	now := time.Now()
+
+	err = client.SetActivity(client.Activity{
+		State:      state,
+		Details:    animeName,
+		LargeImage: animeImage,
+		LargeText:  animeName,
+		SmallImage: smallImage,
+		SmallText:  "Gophimation",
+		Timestamps: &client.Timestamps{
+			Start: &now,
+		},
+		Buttons: []*client.Button{
+			{
+				Label: "Website",
+				Url:   "https://gophimation.vercel.app/",
+			},
+		},
+	})
+
+	if err != nil {
+		panic(err)
+	}
+}
