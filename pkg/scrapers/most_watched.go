25cab81b8bda52f230212e31467824b0e585f75a
Rafael Caixeta <caixetadev@gmail.com>
2024-01-06 23:06:59 +0100

    refactor: most watched scraper

--- pkg/scrapers/most_watched.go
+++ pkg/scrapers/most_watched.go
@@ -12,6 +12,8 @@ import (
 	"github.com/gocolly/colly"
 )
 
+const userPromptMostWatched = "\nColoque um numero para assistir"
+
 // MostWatched prints the most viewed anime of the week and returns its url
 func MostWatched() string {
 	var selectedOption int
@@ -20,23 +22,25 @@ func MostWatched() string {
 
 	var anime []entity.Anime
 
-	c.OnHTML(".highlights .highlight-card .highlight-body", func(h *colly.HTMLElement) {
-		fmt.Printf("[%02d] - %v\n", h.Index+1, h.ChildText(".highlight-title h3"))
-
-		anime = append(anime, entity.Anime{URL: strings.TrimPrefix(h.ChildAttr("a", "href"), constants.URL_BASE)})
-	})
+	setCollyCallbacksMostWatched(c, &anime)
 
 	if err := c.Visit(constants.URL_BASE); err != nil {
 		log.Fatal(err)
 	}
 
-	fmt.Println("\ncoloque um numero para assistir")
-
-	fmt.Scanln(&selectedOption)
+	selectedOption = utils.GetUserInput(userPromptMostWatched)
 
 	utils.OptionIsValid(anime, selectedOption)
 
 	utils.Clear()
 
 	return anime[selectedOption-1].URL
+}
+
+func setCollyCallbacksMostWatched(c *colly.Collector, anime *[]entity.Anime) {
+	c.OnHTML(".highlights .highlight-card .highlight-body", func(h *colly.HTMLElement) {
+		fmt.Printf("[%02d] - %v\n", h.Index+1, h.ChildText(".highlight-title h3"))
+
+		*anime = append(*anime, entity.Anime{URL: strings.TrimPrefix(h.ChildAttr("a", "href"), constants.URL_BASE)})
+	})
 }
